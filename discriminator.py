9c1f56747fc7fb5b5f835350986441db7e633463
Erling Tømte <erling.tmte@gmail.com>
2020-09-10 16:12:36 +0200

    improvements

--- discriminator.py
+++ discriminator.py
@@ -51,8 +51,9 @@ class DiscriminatorNet(nn.Module):
 
 class Discriminator:
     def __init__(self, learning_rate):
-        self.model = DiscriminatorNet(256, 256, 3)
+        self.model = DiscriminatorNet(512, 512, 3)
         #self.images = ["drawing", "iconography", "painting", "sculpture"]
+        self.images = ["alfred", "leonardo", "pablo", "rembrandt"]
         alfred =  glob.glob("/Users/Erling/Documents/PaintingGAN/Painters/Alfred_Sisley/*")
         leonardo =  glob.glob("/Users/Erling/Documents/PaintingGAN/Painters/Leonardo_da_Vinci/*")
         pablo =  glob.glob("/Users/Erling/Documents/PaintingGAN/Painters/Pablo_Picasso/*")
@@ -61,7 +62,7 @@ class Discriminator:
         self.optimizer = optim.Adam(self.model.parameters() ,lr = learning_rate)
         self.criterion = nn.MSELoss()
 
-        self.list_of_paths = {"alfred": alfred, "leonard": leonardo, "pablo": pablo, "rembrandt": rembrandt}
+        self.list_of_paths = {"alfred": alfred, "leonardo": leonardo, "pablo": pablo, "rembrandt": rembrandt}
         self.batch_images = {}
         self.batch_labels = {}
         self.batch_path = {}
@@ -77,7 +78,7 @@ class Discriminator:
         pablo =  glob.glob("/Users/Erling/Documents/PaintingGAN/Painters/Pablo_Picasso/*")
         rembrandt =  glob.glob("/Users/Erling/Documents/PaintingGAN/Painters/Rembrandt/*")
 
-        self.list_of_paths = {"alfred": alfred, "leonard": leonardo, "pablo": pablo, "rembrandt": rembrandt}
+        self.list_of_paths = {"alfred": alfred, "leonardo": leonardo, "pablo": pablo, "rembrandt": rembrandt}
 
     def predict(self, image):
         return self.model(image)
@@ -85,21 +86,22 @@ class Discriminator:
     def load_weights(self, path):
         self.model.load_state_dict(torch.load(path))
         self.name = path
+
     def save_weights(self, path):
         torch.save(self.model.state_dict(), path)
 
     def predict_test(self, imagePath):
-        size= 256, 256
+        size= 512, 512
         im = Image.open(imagePath)
         im.thumbnail(size, Image.ANTIALIAS)
         im = np.array(im)
-        im = cv2.resize(im, (256, 256)) 
+        im = cv2.resize(im, (512, 512)) 
         im = torch.from_numpy(im)
         im = im.transpose(0,-1)
         im = im[None, :, :]
 
         output = self.model(im)
-        predicted = torch.round(output.data[0])
+        predicted = (output.data[0])
         if np.argmax(predicted) == 0:
             print("Bildet er en alfred")
         elif np.argmax(predicted) == 1:
@@ -108,49 +110,49 @@ class Discriminator:
             print("Bildet er en pablo")
         elif np.argmax(predicted) == 3:
             print("Bildet er en Rembrand")
-        print(output)
+        print(predicted)
+        return predicted
 
 
     def load_images(self):
-        size= 256, 256
+        size= 512, 512
         counter = 0
-        for i in range(100):
+        for i in range(32):
             group = random.choice(self.images)
-
             while not self.list_of_paths[group]:
                 self.images.remove(group)
                 group = random.choice(self.images)
-
+                print("sletta")
             imagePath = np.random.choice(self.list_of_paths[group])
             # load the image, pre-process it, and store it in the data list
             im = Image.open(imagePath)
             im.thumbnail(size, Image.ANTIALIAS)
             im = np.array(im)
-            im = cv2.resize(im, (256, 256)) 
+            im = cv2.resize(im, (512, 512)) 
             im = torch.from_numpy(im)
             im = im.transpose(0,-1)
             im = im[None, :, :]
 
-            while im.size() != torch.Size([1, 3, 256, 256]):
+            while im.size() != torch.Size([1, 3, 512, 512]):
                 imagePath = np.random.choice(self.list_of_paths[group])
                 # load the image, pre-process it, and store it in the data list
                 im = Image.open(imagePath)
                 im.thumbnail(size, Image.ANTIALIAS)
                 im = np.array(im)
-                im = cv2.resize(im, (256, 256)) 
+                im = cv2.resize(im, (512, 512)) 
                 im = torch.from_numpy(im)
                 im = im.transpose(0,-1)
                 im = im[None, :, :]
 
             label = np.zeros(4)
 
-            if group == "painting":
+            if group == "alfred":
                 label[0] = 1
-            elif group == "iconography":
+            elif group == "leonardo":
                 label[1] = 1
-            elif group == "sculpture":
+            elif group == "pablo":
                 label[2] = 1
-            elif group == "drawing":
+            elif group == "rembrandt":
                 label[3] = 1
 
 
@@ -163,14 +165,14 @@ class Discriminator:
     def train(self, number_of_batches, number_of_epochs):
         loss_list = []
         acc_list = []
-        batch_size = 100
+        batch_size = 32
         for epoch in range(number_of_epochs):
             for b in range(number_of_batches):
                 correct = 0
                 number_of_paintings = 0
                 for i in range(batch_size):
                     # Run the forward pass
-                    if self.batch_images[str(i)].size() != torch.Size([1, 3, 256, 256]):
+                    if self.batch_images[str(i)].size() != torch.Size([1, 3, 512, 512]):
                         print(self.batch_path[str(i)])
                     output = self.model(self.batch_images[str(i)])
                     
@@ -186,14 +188,16 @@ class Discriminator:
                     loss.backward()
                     self.optimizer.step()
 
+                
+
                     # Track the accuracy
-                    total = 100
+                    total = 32
                     predicted = torch.round(output.data[0])
                     if np.argmax(predicted) == np.argmax(label):
                         correct+= 1
                     acc_list.append(correct / total)
 
-                if (i + 1) % 100 == 0:
+                if (i + 1) % 32 == 0:
                         print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
                                 .format(epoch + 1, number_of_epochs, b , number_of_batches, loss.item(),
                                         (correct / total) * 100))
@@ -202,7 +206,7 @@ class Discriminator:
                 self.load_images()
             self.reset_epoch()
 
-        self.save_weights("discriminator")
+        self.save_weights("discriminator1")
 
 
 
@@ -211,11 +215,97 @@ def Mona_Lisa_Testen(Discriminator):
     print(Discriminator.predict_test(image))
     
 
-DClassifier = Discriminator(0.000046)
-DClassifier.load_images()
-DClassifier.train(20, 12)
+def evaluation(Discriminator):
 
-Mona_Lisa_Testen(DClassifier)
+    alfred =  glob.glob("/Users/Erling/Documents/PaintingGAN/test/Alfred_Sisley/*")
+    leonardo =  glob.glob("/Users/Erling/Documents/PaintingGAN/test/Leonardo_da_Vinci/*")
+    pablo =  glob.glob("/Users/Erling/Documents/PaintingGAN/test/Pablo_Picasso/*")
+    rembrandt =  glob.glob("/Users/Erling/Documents/PaintingGAN/test/Rembrandt/*")
+    groups = ["alfred", "leonardo", "pablo", "rembrandt"]
+    list_of_paths = {"alfred": alfred, "leonardo": leonardo, "pablo": pablo, "rembrandt": rembrandt}
+    size= 512, 512
+    counter = 0
+    batch_images = {}
+    batch_labels = {}
+    batch_path = {}
+    for group in groups:
+        for imagePath in list_of_paths[group]:
+            # load the image, pre-process it, and store it in the data list
+            im = Image.open(imagePath)
+            im.thumbnail(size, Image.ANTIALIAS)
+            im = np.array(im)
+            im = cv2.resize(im, (512, 512)) 
+            im = torch.from_numpy(im)
+            im = im.transpose(0,-1)
+            im = im[None, :, :]
+
+            while im.size() != torch.Size([1, 3, 512, 512]):
+                imagePath = np.random.choice(list_of_paths[group])
+                # load the image, pre-process it, and store it in the data list
+                im = Image.open(imagePath)
+                im.thumbnail(size, Image.ANTIALIAS)
+                im = np.array(im)
+                im = cv2.resize(im, (512, 512)) 
+                im = torch.from_numpy(im)
+                im = im.transpose(0,-1)
+                im = im[None, :, :]
+
+            label = np.zeros(4)
+
+            if group == "alfred":
+                label[0] = 1
+            elif group == "leonardo":
+                label[1] = 1
+            elif group == "pablo":
+                label[2] = 1
+            elif group == "rembrandt":
+                label[3] = 1
+
+
+
+            batch_images.update({str(counter): im})
+            batch_labels.update({str(counter): label})
+            batch_path.update({str(counter): imagePath})
+            counter+= 1
+    
+    print("Cross validation:")
+    correct = 0
+    total = 0
+    alfred_error = 0
+    leo_error = 0
+    pablo_error = 0
+    rembrandt_error = 0
+    for i in range(16*4):
+        output = DClassifier.model(batch_images[str(i)]).detach()
+        predicted = np.argmax(output)
+        label = batch_labels[str(i)]
+        label = np.argmax(torch.Tensor([label]))
+        
+        
+        if predicted == label:
+            correct += 1
+        else:
+            if label == 0:
+                alfred_error +=1
+            elif label == 1:
+                leo_error+=1 
+            elif label == 2:
+                pablo_error +=1
+            elif label == 3:
+                rembrandt_error += 1
+        total += 1
+    print("Cross validation:",correct/(total))
+    print("The agent managed:", correct, "out of a total of:", total)        
+    print("Errors in Alfred images:", alfred_error, "out of 16")
+    print("Errors in Da Vinci images:", leo_error, "out of 16")
+    print("Errors in Picasso images:", pablo_error, "out of 16")
+    print("Errors in Rembrandt images:", rembrandt_error, "out of 16")
+
+DClassifier = Discriminator(0.000146)
+evaluation(DClassifier)
+DClassifier.load_images()
+DClassifier.load_weights("discriminator1")
+evaluation(DClassifier)
 
 #DClassifier.load_images()
 
