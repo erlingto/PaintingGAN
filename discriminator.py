62eab26bb6160277f7194ae5c103322090e3c791
erlingto <erling.tmte@gmail.com>
2020-10-14 20:52:16 +0200

    some improvements

--- discriminator.py
+++ discriminator.py
@@ -21,20 +21,29 @@ class DiscriminatorNet(nn.Module):
     def __init__(self, img_rows, img_cols, channels):
         super(DiscriminatorNet, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels = 3,out_channels = 6, kernel_size = 3, stride= 2, padding= 1)
+        self.conv1 = nn.Conv2d(in_channels = 3,out_channels = 32, kernel_size = 11, stride= 3, padding= 2)
         self.pool1 = nn.MaxPool2d(3,2, padding= 1)
-        self.conv2 = nn.Conv2d(in_channels = 6,out_channels = 12, kernel_size = 3, stride= 2, padding= 1)
-        self.pool2 = nn.MaxPool2d(3,2, padding= 1)
-        self.conv3 = nn.Conv2d(in_channels = 12,out_channels = 24, kernel_size = 3, stride= 2, padding= 1)
-        self.pool3 = nn.MaxPool2d(3,2, padding= 1)
-        self.conv_output_H = calculate_conv_output(img_rows, 3, 1, 2)
-        self.conv_output_W = calculate_conv_output(img_cols, 3, 1, 2)
+        self.conv2 = nn.Conv2d(in_channels = 32 ,out_channels = 96, kernel_size = 3, stride= 2, padding= 1)
+        self.pool2 = nn.MaxPool2d(2,2, padding= 1)
+        self.conv3 = nn.Conv2d(in_channels = 96 ,out_channels = 192, kernel_size = 3, stride= 2, padding= 1)
+        self.pool3 = nn.MaxPool2d(2,2, padding= 1)
+        """ CONV DIMENSIONS CALCULATIONS """
+        self.conv_output_H = calculate_conv_output(img_rows, 11, 2, 3)
+        self.conv_output_W = calculate_conv_output(img_cols, 11, 2, 3)
+        """ POOLING DIMENSIONS CALCULATIONS """
+        self.conv_output_H = calculate_conv_output(self.conv_output_H , 3, 1, 2)
+        self.conv_output_W = calculate_conv_output(self.conv_output_W , 3, 1, 2)
         
-        for _ in range(5):
+        for _ in range(2):
             self.conv_output_H = calculate_conv_output(self.conv_output_H , 3, 1, 2)
             self.conv_output_W = calculate_conv_output(self.conv_output_W , 3, 1, 2)
-        self.fc1 = torch.nn.Linear(calculate_flat_input(1, self.conv_output_H, self.conv_output_W)*24,  512)
-        self.fc2 = torch.nn.Linear(512, 4)
+            self.conv_output_H = calculate_conv_output(self.conv_output_H , 2, 1, 2)
+            self.conv_output_W = calculate_conv_output(self.conv_output_W , 2, 1, 2)
+        
+        self.classifier = nn.Sequential(
+            torch.nn.Linear(calculate_flat_input(1, self.conv_output_H, self.conv_output_W)*192,  512), nn.ReLU(True),
+            nn.Dropout(), nn.Linear(512, 4),)
+
         
 
     def forward(self, x):
@@ -44,9 +53,8 @@ class DiscriminatorNet(nn.Module):
         x=self.pool2(x)
         x = F.relu(self.conv3(x.float()))
         x=self.pool3(x)
-        x = x.view(-1, calculate_flat_input(1, self.conv_output_H, self.conv_output_W)*24)
-        x= F.relu(self.fc1(x))
-        x= self.fc2(x)
+        x = x.view(-1, calculate_flat_input(1, self.conv_output_H, self.conv_output_W)*192)
+        x= self.classifier(x)
         return x
 
 class Discriminator:
@@ -71,6 +79,7 @@ class Discriminator:
         self.big_batch_images = {}
         self.big_batch_labels = {}
         self.batch_path = {}
+        self.images = ["alfred", "leonardo", "leonardo", "pablo", "rembrandt"]
 
     def reset_epoch(self): 
         alfred =  glob.glob("/Users/Erling/Documents/PaintingGAN/Painters/Alfred_Sisley/*")
@@ -79,7 +88,7 @@ class Discriminator:
         rembrandt =  glob.glob("/Users/Erling/Documents/PaintingGAN/Painters/Rembrandt/*")
 
         self.list_of_paths = {"alfred": alfred, "leonardo": leonardo, "pablo": pablo, "rembrandt": rembrandt}
-
+        self.images = ["alfred", "leonardo", "leonardo", "pablo", "rembrandt"]
     def predict(self, image):
         return self.model(image)
 
@@ -160,7 +169,6 @@ class Discriminator:
             self.batch_labels.update({str(counter): label})
             self.batch_path.update({str(counter): imagePath})
             counter+= 1
-            self.list_of_paths[group].remove(imagePath)
     
     def train(self, number_of_batches, number_of_epochs):
         loss_list = []
@@ -169,7 +177,7 @@ class Discriminator:
         for epoch in range(number_of_epochs):
             for b in range(number_of_batches):
                 correct = 0
-                number_of_paintings = 0
+                number_of_leo_paintings = 0
                 for i in range(batch_size):
                     # Run the forward pass
                     if self.batch_images[str(i)].size() != torch.Size([1, 3, 512, 512]):
@@ -177,8 +185,8 @@ class Discriminator:
                     output = self.model(self.batch_images[str(i)])
                     
                     label = self.batch_labels[str(i)]
-                    if label[0] == 1:
-                        number_of_paintings += 1
+                    if label[1] == 1:
+                        number_of_leo_paintings += 1
                     label = torch.Tensor([label])
                     loss = self.criterion(output, label)
                     loss_list.append(loss.item())
@@ -201,7 +209,7 @@ class Discriminator:
                         print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
                                 .format(epoch + 1, number_of_epochs, b , number_of_batches, loss.item(),
                                         (correct / total) * 100))
-                        print(number_of_paintings)
+                        print(number_of_leo_paintings)
                 self.reset_batches()
                 self.load_images()
             self.reset_epoch()
@@ -227,7 +235,7 @@ def evaluation(Discriminator):
     counter = 0
     batch_images = {}
     batch_labels = {}
-    batch_path = {}
+    error_results = {}
     for group in groups:
         for imagePath in list_of_paths[group]:
             # load the image, pre-process it, and store it in the data list
@@ -265,7 +273,7 @@ def evaluation(Discriminator):
 
             batch_images.update({str(counter): im})
             batch_labels.update({str(counter): label})
-            batch_path.update({str(counter): imagePath})
+            
             counter+= 1
     
     print("Cross validation:")
@@ -281,29 +289,40 @@ def evaluation(Discriminator):
         label = batch_labels[str(i)]
         label = np.argmax(torch.Tensor([label]))
         
-        
+
         if predicted == label:
             correct += 1
         else:
             if label == 0:
                 alfred_error +=1
+                error_results.update({("alfred" + str(alfred_error)) : output } )
             elif label == 1:
                 leo_error+=1 
+                error_results.update({("Da Vinci" + str(leo_error)) : output } )
             elif label == 2:
                 pablo_error +=1
+                error_results.update({("Picasso" + str(pablo_error)) : output } )
             elif label == 3:
                 rembrandt_error += 1
+                error_results.update({("Rembrandt" + str(rembrandt_error)) : output } )
+        
         total += 1
     print("Cross validation:",correct/(total))
-    print("The agent managed:", correct, "out of a total of:", total)        
+    print("The agent managed", correct, "out of a total of:", total)        
     print("Errors in Alfred images:", alfred_error, "out of 16")
     print("Errors in Da Vinci images:", leo_error, "out of 16")
     print("Errors in Picasso images:", pablo_error, "out of 16")
     print("Errors in Rembrandt images:", rembrandt_error, "out of 16")
 
+
+    print ("-----------------------ERRORS-----------------------")
+    for error in error_results:
+        print(error ,":", error_results[error])
+
 DClassifier = Discriminator(0.000146)
 evaluation(DClassifier)
 DClassifier.load_images()
+DClassifier.train(16, 55)
 DClassifier.load_weights("discriminator1")
 evaluation(DClassifier)
 
